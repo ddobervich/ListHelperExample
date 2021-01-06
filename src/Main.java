5fde2843493a4a73e9acecf59742231c5b179a81
David Dobervich <david.dobervich+apcsgithub@gmail.com>
2021-01-06 14:52:28 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // example:  make a list from strings
+        ArrayList<String> names = ListHelper.makeList("bob", "jim", "sally");
+
+        System.out.println(names);
+    }
+}
